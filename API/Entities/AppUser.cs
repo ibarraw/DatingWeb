aa70c0aa6e812511816bdcc8abf7da16fa7019bd
William Ibarra <ibarraw@sheridancollege.ca>
2024-01-28 13:59:50 -0500

    Added Authentication

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,6 +1,8 @@
 ﻿namespace API;
 public class AppUser
 {
-  public int Id { get; set; }
-  public string UserName { get; set; }
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
 }
